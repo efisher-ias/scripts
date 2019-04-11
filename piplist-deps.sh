ae8d0c5cc4c186f20bfba83969f550c5abfa9214
EFisher <45256225+efisher-ias@users.noreply.github.com>
2019-04-11 11:23:25 +0300

    Update piplist-deps.sh

--- piplist-deps.sh
+++ piplist-deps.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-for line in $( pip-module-scanner -p $1 | awk '{split($1,a,"=="); print a[1];}' ); do  pipdeptree -p $line -w silence -f | awk '{ gsub(/^[ \t]+/,"",$1); print $1; }'; done
+PROJECT_FOLDER = $1
+for line in $( pip-module-scanner -p $PROJECT_FOLDER | awk '{split($1,a,"=="); print a[1];}' ); do  
+  pipdeptree -p $line -w silence -f | awk '{ gsub(/^[ \t]+/,"",$1); print $1; }'; 
+done
